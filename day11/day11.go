ea97c9c22f3f38ac5e46ebfd8b5ebb05ee789a35
Felix Achter <felix.achter.it@gmail.com>
2022-12-11 17:58:30 +0100

    day11 - 1

---
+++ day11/day11.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Monkey struct {
+	items []int
+	operation string
+	secondValue int
+	testDivider int
+	trueNext int
+	falseNext int
+	totalInspections int
+}
+
+
+func main() {
+
+	partOne("day11-test.txt")
+	partOne("day11.txt")
+}
+
+func partOne(fileName string) {
+	monkeys := getMonkeys(fileName)
+	SimulateRounds(monkeys)
+	printScore(monkeys)
+}
+
+func printScore(monkeys map[int]*Monkey) {
+	for _, m := range monkeys {
+		fmt.Println(m.totalInspections)
+	}
+	fmt.Println()
+}
+
+func getMonkeys(fileName string) map[int]*Monkey {
+	file, _ := os.Open("day11/" + fileName)
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+	monkeys := map[int]*Monkey{}
+	currentIndex := 0
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		if strings.Contains(line, "Monkey") {
+			indexWithColon := strings.Fields(line)[1]
+			index, _ := strconv.Atoi(indexWithColon[:len(indexWithColon)-1])
+			currentIndex = index
+			monkeys[currentIndex] = &Monkey{}
+		} else if strings.Contains(line, "Starting items") {
+			startingItems := strings.Split(strings.Split(line, ": ")[1], ", ")
+			for _, item := range startingItems {
+				score, _ := strconv.Atoi(item)
+				monkeys[currentIndex].items = append(monkeys[currentIndex].items, score)
+			}
+		} else if strings.Contains(line, "Operation") {
+			monkeys[currentIndex].operation = strings.Split(line, "= ")[1]
+		} else if strings.Contains(line, "Test") {
+			monkeys[currentIndex].testDivider = getLastFieldAsInt(line)
+		} else if strings.Contains(line, "If true") {
+			monkeys[currentIndex].trueNext = getLastFieldAsInt(line)
+		} else if strings.Contains(line, "If false") {
+			monkeys[currentIndex].falseNext = getLastFieldAsInt(line)
+		}
+	}
+	return monkeys
+}
+
+func SimulateRounds(monkeys map[int]*Monkey) {
+	for i := 0; i < 20; i++ {
+		for index := 0; index < len(monkeys); index++ {
+			monkeyItems := monkeys[index].items
+			for _, item := range monkeyItems {
+				monkeys[index].totalInspections++
+				newValue := calc(monkeys[index].operation, item)
+				newValue /= 3
+				if newValue%monkeys[index].testDivider == 0 {
+					monkeys[monkeys[index].trueNext].items = append(monkeys[monkeys[index].trueNext].items, newValue)
+				} else {
+					monkeys[monkeys[index].falseNext].items = append(monkeys[monkeys[index].falseNext].items, newValue)
+				}
+				monkeys[index].items = monkeys[index].items[1:]
+			}
+		}
+	}
+}
+
+func getLastFieldAsInt(line string) int {
+	fields := strings.Fields(line)
+	lastFieldInt, _ := strconv.Atoi(fields[len(fields)-1])
+	return lastFieldInt
+}
+
+func calc(val string, x int) int {
+	ops := map[string]func(int, int) int{
+		"+": func(a, b int) int { return a + b },
+		"-": func(a, b int) int { return a - b },
+		"*": func(a, b int) int { return a * b },
+		"/": func(a, b int) int { return a / b },
+	}
+	fields := strings.Fields(val)
+	val1 := x
+	val2 := x
+	if fields[0] != "old" {
+		val1, _ = strconv.Atoi(fields[0])
+	}
+	if fields[2] != "old" {
+		val2, _ = strconv.Atoi(fields[2])
+	}
+	return ops[fields[1]](val1, val2)
+}
+
+
+
